56374efa0e89b26162cdec5d7378f749b63845d8
Luisa SanMartin <luisasm@uber.com>
2018-10-20 13:10:44 -0400

    Removing unnecessary code

--- searchGoogle.py
+++ searchGoogle.py
@@ -2,7 +2,6 @@ from googlesearch import search
 BASE_URL = "https://stackoverflow.com/questions/"
 def searchGoogle (aQuestion):
     # This function wll search Google and take the top Stack Overflow result and return the questionID (in the url)
-    # GoogleSearchEngineKey = "AIzaSyDXEd0q3hdpn_ckTx4lsmJV_AcVpEqP1s4"
 	questionID = -1
 	for url in search(aQuestion, stop=5):
 		if BASE_URL not in url:
@@ -13,6 +12,3 @@ def searchGoogle (aQuestion):
 	if questionID == -1:
 		raise "Could not find any questions"
 	return questionID
-
-qID = searchGoogle('how to align two text boxes vertically html')
-print(qID)
