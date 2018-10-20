fd090a7a546b76420d0bf726c280e53eb963310a
Adam Weinstein <adam@66-207-220-250.beanfield.net>
2018-10-20 14:15:50 -0400

    Got xml body of top answer for a given question Id

--- getAnswer.py
+++ getAnswer.py
@@ -1,16 +1,9 @@
 import requests
 
-def getAnswer(questionID):
-    #This function uses the Stack Overflow API to return the answer for a specific question ID.
-    answerId = 25491297
-    url = "https://api.stackexchange.com/2.2/answers/%s?order=desc&sort=activity&site=stackoverflow&filter=withbody" % (answerId)
-
+def getAnswer(questionId):
+    #This function uses the Stack Overflow API to return the top rated answer in xml format for a specific question ID.
+    url = "https://api.stackexchange.com/2.2/questions/%s/answers?order=desc&sort=votes&site=stackoverflow&filter=withbody" % (questionId)
     r = requests.get(url)
-
-    print(r)
-
-    answer = "1"
+    data = r.json()
+    answer = data['items'][0]['body']
     return answer
-
-result = getAnswer(123)
-print(result)
