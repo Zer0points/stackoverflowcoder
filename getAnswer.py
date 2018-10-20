ddf89d5fac446dbd46fe13dcd6816e57e4718491
Adam Weinstein <adam@66-207-220-250.beanfield.net>
2018-10-20 13:09:43 -0400

    Got good API response

--- getAnswer.py
+++ getAnswer.py
@@ -1,4 +1,16 @@
+import requests
+
 def getAnswer(questionID):
     #This function uses the Stack Overflow API to return the answer for a specific question ID.
-    answer = ""
+    answerId = 25491297
+    url = "https://api.stackexchange.com/2.2/answers/%s?order=desc&sort=activity&site=stackoverflow&filter=withbody" % (answerId)
+
+    r = requests.get(url)
+
+    print(r)
+
+    answer = "1"
     return answer
+
+result = getAnswer(123)
+print(result)
