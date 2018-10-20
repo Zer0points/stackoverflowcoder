d766106e763fdbf7b2922df51da9b6580d2aecaf
Adam Weinstein <adam@66-207-220-250.beanfield.net>
2018-10-20 14:35:15 -0400

    Added spaces for master Luisa

--- getAnswer.py
+++ getAnswer.py
@@ -1,12 +1,12 @@
 import requests
 
 def getAnswer(questionId):
-    #This function uses the Stack Overflow API to return the top rated answer in xml format for a specific question ID.
+    # This function uses the Stack Overflow API to return the top rated answer in xml format for a specific question ID.
     url = "https://api.stackexchange.com/2.2/questions/%s/answers?order=desc&sort=votes&site=stackoverflow&filter=withbody" % (questionId)
     r = requests.get(url)
     data = r.json()
     
-    #Exception for no answers
+    # Exception for no answers
     if len(data['items']) == 0:
     	raise ValueError("No answers for this question")
 
